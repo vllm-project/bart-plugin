38c970416a927d4514bb3a16d21a3f57cf21d790
NickLucche <nlucches@redhat.com>
2026-04-08 09:21:40 +0200

    version bump
    Signed-off-by: NickLucche <nlucches@redhat.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="vllm-bart-plugin",
-    version="0.3.1",
+    version="0.3.2",
     description="BART model plugin for vLLM",
     author="Nicolò Lucchesi",
     author_email="nick.lucche@redhat.com",
